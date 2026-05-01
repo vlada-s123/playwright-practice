a0f5f09dc1d687da5016351ce58bab9033e994fc
vlada-s <vlada.shyshkovska@gmail.com>
2026-05-01 11:55:10 +0300

    fix for assertion url

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -6,7 +6,7 @@ test ('Login test', async ({page}) => {
     await page.locator('#password').fill('welcome01');
     await page.getByTestId('login-submit').click();
 
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login');
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
     await expect(page.getByTestId('page-title')).toHaveText('My account');
     await expect(page.getByTestId('nav-menu')).toHaveText('Jane Doe');
 
